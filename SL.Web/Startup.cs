3e10dc99cad1ee15cc7d410bd860f92e8a00abee
Nestor Oviedo <noviedo@rosen-group.com>
2020-01-28 16:59:09 -0500

    Basic functionality working

--- SL.Web/Startup.cs
+++ SL.Web/Startup.cs
@@ -25,7 +25,7 @@ namespace Sl.Web
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "ClientApp/dist/ClientApp";
             });
         }
 
